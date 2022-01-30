f1db142b9c79b163b587450239757a0f856d4814
Faustyna Mika <faustyna.mika@gmail.com>
2022-01-30 19:10:37 +0100

    Ostylowanie komponentów w zadaniu 1

--- client/src/ui/atoms/NoContent.jsx
+++ client/src/ui/atoms/NoContent.jsx
@@ -1,3 +1,19 @@
+import { Box, Typography } from '@mui/material';
+import noContent from '../../assets/no_content.png'
+import { theme } from '../../theme';
+
 export const NoContent = () => {
-  return null;
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <img src={noContent} />
+      <Typography sx={{ color: theme.palette.grey[400] }}>Brak danych do wyświetlenia</Typography>
+    </Box>
+  );
 };
